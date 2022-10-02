20975e34ac4d87e23ef66323c569cbf6bcfd53f8
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-10-02 09:42:16 +0300

    Update task_03_football_cards.py

--- 03 Lists Basics/03-Exercise/task_03_football_cards.py
+++ 03 Lists Basics/03-Exercise/task_03_football_cards.py
@@ -1,16 +1,17 @@
-team_A_count = 11
-team_B_count = 11
-cards = input()
-cards_list = cards.split(" ")
+team_a_count = 11
+team_b_count = 11
+cards_list = input().split(" ")
 cards_list = list(dict.fromkeys(cards_list))
+game_was_terminated = False
 for i in range(len(cards_list)):
     current_player = cards_list[i]
     if current_player[0] == "A":
-        team_A_count -= 1
+        team_a_count -= 1
     elif current_player[0] == "B":
-        team_B_count -= 1
-    if team_A_count < 7 or team_B_count < 7:
+        team_b_count -= 1
+    if team_a_count < 7 or team_b_count < 7:
+        game_was_terminated = True
         break
-print(f"Team A - {team_A_count}; Team B - {team_B_count}")
-if team_A_count < 7 or team_B_count < 7:
+print(f"Team A - {team_a_count}; Team B - {team_b_count}")
+if game_was_terminated:
     print("Game was terminated")
