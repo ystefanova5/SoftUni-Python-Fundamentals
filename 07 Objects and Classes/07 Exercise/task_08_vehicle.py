a0ec81b36a7e164eae1c57d5000fb7346eda3bd7
ystefanova5 <110626738+ystefanova5@users.noreply.github.com>
2022-10-28 20:02:22 +0300

    Added 2 tasks to Objects and Classes/Exercise

---
+++ 07 Objects and Classes/07 Exercise/task_08_vehicle.py
@@ -0,0 +1,29 @@
+class Vehicle:
+    def __init__(self, type, model, price):
+        self.type = type
+        self.model = model
+        self.price = price
+        self.owner = None
+
+    def buy(self, money, owner):
+        if money >= self.price:
+            if self.owner is None:
+                self.owner = owner
+                change = money - self.price
+                return f"Successfully bought a {self.type}. Change: {change:.2f}"
+            else:
+                return "Car already sold"
+        else:
+            return "Sorry, not enough money"
+
+    def sell(self):
+        if self.owner is not None:
+            self.owner = None
+        else:
+            return "Vehicle has no owner"
+
+    def __repr__(self):
+        if self.owner is not None:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        else:
+            return f"{self.model} {self.type} is on sale: {self.price}"
