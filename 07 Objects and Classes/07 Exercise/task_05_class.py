471420fb43f652f977e29cc15a5fd5bb18476c76
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-10-29 14:26:57 +0300

    Update task_05_class.py

--- 07 Objects and Classes/07 Exercise/task_05_class.py
+++ 07 Objects and Classes/07 Exercise/task_05_class.py
@@ -8,7 +8,7 @@ class Class:
         self.average_grade = 0
 
     def add_student(self, name, grade):
-        if len(self.students) < self.__students_count:
+        if len(self.students) < Class.__students_count:
             self.students.append(name)
             self.grades.append(grade)
 
