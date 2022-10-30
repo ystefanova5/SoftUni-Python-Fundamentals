65b47a5d7cd3387ff8d073f0644b348d223c4542
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-10-30 22:17:45 +0200

    Update task_03_statistics.py

--- 08 Dictionaries/08 Lab/task_03_statistics.py
+++ 08 Dictionaries/08 Lab/task_03_statistics.py
@@ -17,16 +17,12 @@ command = input()
 while command != "statistics":
     current_key, current_value = command.split(": ")
     if current_key in bakery_stock:
-        bakery_stock[current_key] += current_value
+        bakery_stock[current_key] += int(current_value)
     else:
-        bakery_stock[current_key] = current_value
+        bakery_stock[current_key] = int(current_value)
     command = input()
-products = 0
-quantity = 0
 print("Products in stock:")
 for (key, value) in bakery_stock.items():
     print(f"- {key}: {value}")
-    products += 1
-    quantity += value
-print(f"Total Products: {products}")
-print(f"Total Quantity: {quantity}")
+print(f"Total Products: {len(bakery_stock)}")
+print(f"Total Quantity: {sum(bakery_stock.values())}")
