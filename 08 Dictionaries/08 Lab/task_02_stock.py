2f3b82d19dd1b03a17d18ff1113701b32b7eab8d
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-10-30 21:49:59 +0200

    Update task_02_stock.py

--- 08 Dictionaries/08 Lab/task_02_stock.py
+++ 08 Dictionaries/08 Lab/task_02_stock.py
@@ -1,3 +1,12 @@
+# Task description:
+# You will be given key-value pairs of products and quantities (on a single line separated by space). 
+# On the following line, you will be given products to search for. Check for each product. 
+# You have 2 possibilities:
+# •	If you have the product, print "We have {quantity} of {product} left".
+# •	Otherwise, print "Sorry, we don't have {product}".
+
+
+
 products = input().split()
 keys = [key for key in products if products.index(key) % 2 == 0]
 values = [int(value) for value in products if products.index(value) % 2 != 0]
