2b2d35d2d7d76851a8f0eca6acdd26a93b367e60
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-10-30 21:47:59 +0200

    Update task_01_bakery.py

--- 08 Dictionaries/08 Lab/task_01_bakery.py
+++ 08 Dictionaries/08 Lab/task_01_bakery.py
@@ -1,3 +1,8 @@
+# You will receive a single line containing some food (keys) and quantities (values). 
+# They will be separated by a single space (the first element is the key, the second – the value, and so on). 
+# Create a dictionary with all the keys and values and print it on the console.
+
+
 items = input().split()
 keys = [key for key in items if items.index(key) % 2 == 0]
 values = [int(value) for value in items if items.index(value) % 2 != 0]
