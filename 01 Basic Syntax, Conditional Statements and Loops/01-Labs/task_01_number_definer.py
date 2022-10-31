d499b035d66294c2726f51c61032ca5f6b160f12
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-10-31 09:21:25 +0200

    Update task_01_number_definer.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Labs/task_01_number_definer.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Labs/task_01_number_definer.py
@@ -1,3 +1,10 @@
+# Number Definer - Task description:
+# Write a program that reads a floating-point number and:
+# -	prints "zero" if the number is zero
+# -	prints "positive" or "negative"
+# -	adds "small" if the absolute value of the number is less than 1 and it is not 0, or "large" if it exceeds 1 000 000
+
+
 number = float(input())
 if number == 0:
     print("zero")
