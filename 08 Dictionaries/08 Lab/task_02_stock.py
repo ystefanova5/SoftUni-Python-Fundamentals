6ff74f744842789a4061decf2343921e72351858
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-01 12:44:50 +0200

    Update task_02_stock.py

--- 08 Dictionaries/08 Lab/task_02_stock.py
+++ 08 Dictionaries/08 Lab/task_02_stock.py
@@ -1,9 +1,10 @@
-# Task description:
+# 02. Stock
+# Task Description:
 # You will be given key-value pairs of products and quantities (on a single line separated by space). 
 # On the following line, you will be given products to search for. Check for each product. 
 # You have 2 possibilities:
-# •	If you have the product, print "We have {quantity} of {product} left".
-# •	Otherwise, print "Sorry, we don't have {product}".
+# •If you have the product, print "We have {quantity} of {product} left".
+# •Otherwise, print "Sorry, we don't have {product}".
 
 
 
