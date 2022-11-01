476b36c6d163c01dbe59c5d8fb9913f733c48d01
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-01 12:44:00 +0200

    Update task_01_bakery.py

--- 08 Dictionaries/08 Lab/task_01_bakery.py
+++ 08 Dictionaries/08 Lab/task_01_bakery.py
@@ -1,4 +1,5 @@
-# Task description:
+# 01. Bakery
+# Task Description:
 # You will receive a single line containing some food (keys) and quantities (values). 
 # They will be separated by a single space (the first element is the key, the second – the value, and so on). 
 # Create a dictionary with all the keys and values and print it on the console.
