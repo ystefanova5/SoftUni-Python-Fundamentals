595186f246b815a649a4d3c97262d82acacdedbe
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-02 16:15:29 +0200

    Update task_04_phonebook.py

--- 08 Dictionaries/08 Exercise/task_04_phonebook.py
+++ 08 Dictionaries/08 Exercise/task_04_phonebook.py
@@ -8,3 +8,19 @@
 # "{name} -> {number}".
 # In case the contact isn't found, print: "Contact {name} does not exist."
 
+
+phonebook = {}
+command = input().split("-")
+while len(command) > 1:
+    current_key = command[0]
+    current_value = command[1]
+    phonebook[current_key] = current_value
+    command = input().split("-")
+
+searched_numbers = int(command[0])
+for number in range(searched_numbers):
+    searched_name = input()
+    if searched_name in phonebook:
+        print(f"{searched_name} -> {phonebook[searched_name]}")
+    else:
+        print(f"Contact {searched_name} does not exist.")
