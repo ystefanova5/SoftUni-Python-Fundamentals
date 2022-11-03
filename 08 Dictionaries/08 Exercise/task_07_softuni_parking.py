461a24c717334fbd67f34f6604b8332ab1d7ffaf
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-03 13:59:20 +0200

    Update task_07_softuni_parking.py

--- 08 Dictionaries/08 Exercise/task_07_softuni_parking.py
+++ 08 Dictionaries/08 Exercise/task_07_softuni_parking.py
@@ -1,9 +1,5 @@
 # 7. SoftUni Parking
 # Task Description:
-# SoftUni just got a new fancy parking lot.
-# It even has online parking validation, except the online service doesn't work.
-# It can only receive users' data, but it doesn't know what to do with it.
-# Good thing you're on the dev team and know how to fix it, right?
 # Write a program, which validates a parking place - users can register to enter the park and unregister to leave.
 # The program receives 2 types of commands:
 # • "register {username} {license_plate_number}":
@@ -28,3 +24,32 @@
 # The input will always be valid, and you do not need to check it explicitly.
 
 
+def register(username, license_plate_number, users):
+    if username in users:
+        return f"ERROR: already registered with plate number {license_plate_number}"
+    else:
+        users[username] = license_plate_number
+        return f"{username} registered {license_plate_number} successfully"
+
+
+def unregister(username, users):
+    if username not in users:
+        return f"ERROR: user {username} not found"
+    else:
+        del users[username]
+        return f"{username} unregistered successfully"
+
+
+parking_users = {}
+number_of_commands = int(input())
+for command in range(number_of_commands):
+    current_command = input().split()
+    current_task = current_command[0]
+    current_username = current_command[1]
+    if current_task == "register":
+        current_license_plate = current_command[2]
+        print(register(current_username, current_license_plate, parking_users))
+    elif current_task == "unregister":
+        print(unregister(current_username, parking_users))
+for user in parking_users:
+    print(f"{user} => {parking_users[user]}")
