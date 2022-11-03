654534984f0539d72f0e3e935491bda25f916df1
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-03 16:07:31 +0200

    Update task_10_company_users.py

--- 08 Dictionaries/08 Exercise/task_10_company_users.py
+++ 08 Dictionaries/08 Exercise/task_10_company_users.py
@@ -15,4 +15,17 @@
 # • The input always will be valid.
 
 
-
+companies = {}
+command = input()
+while command != "End":
+    current_company = command.split(" -> ")[0]
+    current_id = command.split(" -> ")[1]
+    if current_company not in companies:
+        companies[current_company] = []
+    if current_id not in companies[current_company]:
+        companies[current_company].append(current_id)
+    command = input()
+for company in companies:
+    print(company)
+    for id in companies[company]:
+        print(f"-- {id}")
