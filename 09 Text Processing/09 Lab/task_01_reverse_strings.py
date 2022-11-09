bd016b73031b3ee831e4778d7d99c3d80ac1cbdb
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-09 11:00:47 +0200

    Update task_01_reverse_strings.py

---
+++ 09 Text Processing/09 Lab/task_01_reverse_strings.py
@@ -0,0 +1,9 @@
+word = input()
+while word != "end":
+    reversed_word = word[::-1]
+    print(f"{word} = {reversed_word}")
+    word = input()
+
+###################################### Task Description ######################################
+You will be given strings on separate lines until you receive an "end" command. 
+Write a program that reverses strings and prints each pair on a separate line in the format "{word} = {reversed_word}".
