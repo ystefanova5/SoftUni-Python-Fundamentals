3956a3fb33fa1391f5cc98fe000bf9b96e7f7ecd
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-09 11:17:02 +0200

    Update task_02_repeat_strings.py

--- 09 Text Processing/09 Lab/task_02_repeat_strings.py
+++ 09 Text Processing/09 Lab/task_02_repeat_strings.py
@@ -6,7 +6,7 @@ print(result_string)
 
 
 ###################################### Task Description ######################################
-2. Repeat Strings
-Write a program that reads a sequence of strings, separated by a single space. 
-Each string should be repeated N times, where N is the length of the string. 
-Print the final strings concatenated into one string.
+# 2. Repeat Strings
+# Write a program that reads a sequence of strings, separated by a single space. 
+# Each string should be repeated N times, where N is the length of the string. 
+# Print the final strings concatenated into one string.
