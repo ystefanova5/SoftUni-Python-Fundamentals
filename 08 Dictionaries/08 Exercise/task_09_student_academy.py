f4476ea4e4efaddbe5b707c3ecc21bf04df89b8e
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-16 22:07:08 +0200

    Update task_09_student_academy.py

--- 08 Dictionaries/08 Exercise/task_09_student_academy.py
+++ 08 Dictionaries/08 Exercise/task_09_student_academy.py
@@ -1,15 +1,3 @@
-# 9. Student Academy
-# Task Description:
-# Write a program that keeps the information about students and their grades.
-# On the first line, you will receive an integer number representing the next pair of rows.
-# On the next lines, you will be receiving each student's name and their grade.
-# Keep track of all grades for each student and keep only the students
-# with an average grade higher than or equal to 4.50.
-# Print the final dictionary with students and their average grade in the following format:
-# "{name} -> {averageGrade}"
-# Format the average grade to the 2nd decimal place.
-
-
 number_of_students = int(input())
 counter = 0
 students_list = []
@@ -28,3 +16,15 @@ for student, grades in students_dict.items():
     if average_grade >= 4.50:
         print(f"{student} -> {average_grade:.2f}")
         
+
+################################################   Task Description   ################################################
+# 9. Student Academy
+# Task Description:
+# Write a program that keeps the information about students and their grades.
+# On the first line, you will receive an integer number representing the next pair of rows.
+# On the next lines, you will be receiving each student's name and their grade.
+# Keep track of all grades for each student and keep only the students
+# with an average grade higher than or equal to 4.50.
+# Print the final dictionary with students and their average grade in the following format:
+# "{name} -> {averageGrade}"
+# Format the average grade to the 2nd decimal place.
