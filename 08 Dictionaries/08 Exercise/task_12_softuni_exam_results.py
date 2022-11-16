d4c66e59395f88042e5f1c17e3245565b87b07f7
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-16 22:03:14 +0200

    Update task_12_softuni_exam_results.py

--- 08 Dictionaries/08 Exercise/task_12_softuni_exam_results.py
+++ 08 Dictionaries/08 Exercise/task_12_softuni_exam_results.py
@@ -1,3 +1,33 @@
+exam_results = {}
+banned = []
+command = input()
+while command != "exam finished":
+    current_submission = command.split("-")
+    if len(current_submission) == 2:
+        current_username = current_submission[0]
+        banned.append(current_username)
+    else:
+        current_username, current_language = current_submission[0], current_submission[1]
+        current_points = int(current_submission[2])
+        if current_language not in exam_results:
+            exam_results[current_language] = {"submissions": 0}
+        if current_username in exam_results[current_language]:
+            if exam_results[current_language][current_username] > current_points:
+                current_points = exam_results[current_language][current_username]
+        exam_results[current_language][current_username] = current_points
+        exam_results[current_language]["submissions"] += 1
+    command = input()
+print("Results:")
+for value in exam_results.values():
+    for person, points in value.items():
+        if person not in banned and person != "submissions":
+            print(f"{person} | {points}")
+print("Submissions:")
+for value, key in exam_results.items():
+    print(f"{value} - {exam_results[value]['submissions']}")
+
+    
+################################################   Task Description   ################################################
 # 12. SoftUni Exam Results
 # Task Description:
 # Judge statistics on the last Programing Fundamentals exam were not working correctly,
@@ -31,32 +61,3 @@
 # • Print the exam results for each participant
 # • After that, print each language in the format shown above
 # • Allowed working time / memory: 100ms / 16MB
-
-
-exam_results = {}
-banned = []
-command = input()
-while command != "exam finished":
-    current_submission = command.split("-")
-    if len(current_submission) == 2:
-        current_username = current_submission[0]
-        banned.append(current_username)
-    else:
-        current_username, current_language = current_submission[0], current_submission[1]
-        current_points = int(current_submission[2])
-        if current_language not in exam_results:
-            exam_results[current_language] = {"submissions": 0}
-        if current_username in exam_results[current_language]:
-            if exam_results[current_language][current_username] > current_points:
-                current_points = exam_results[current_language][current_username]
-        exam_results[current_language][current_username] = current_points
-        exam_results[current_language]["submissions"] += 1
-    command = input()
-print("Results:")
-for value in exam_results.values():
-    for person, points in value.items():
-        if person not in banned and person != "submissions":
-            print(f"{person} | {points}")
-print("Submissions:")
-for value, key in exam_results.items():
-    print(f"{value} - {exam_results[value]['submissions']}")
