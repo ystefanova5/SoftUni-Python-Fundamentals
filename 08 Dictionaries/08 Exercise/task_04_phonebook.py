503b6c7df45c4cafd0af1986d665ef3ebc96abbf
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-16 22:13:12 +0200

    Update task_04_phonebook.py

--- 08 Dictionaries/08 Exercise/task_04_phonebook.py
+++ 08 Dictionaries/08 Exercise/task_04_phonebook.py
@@ -1,14 +1,3 @@
-# 4. Phonebook
-# Task Description:
-# Write a program that receives info from the console about people and their phone numbers.
-# Each entry should have a name and a number (both strings) separated by a "-".
-# If you receive a name that already exists in the phonebook, update its number.
-# After filling the phonebook, you will receive a number – N.
-# Your program should be able to perform a search of contact by name and print its details in the format:
-# "{name} -> {number}".
-# In case the contact isn't found, print: "Contact {name} does not exist."
-
-
 phonebook = {}
 command = input().split("-")
 while len(command) > 1:
@@ -24,3 +13,14 @@ for number in range(searched_numbers):
         print(f"{searched_name} -> {phonebook[searched_name]}")
     else:
         print(f"Contact {searched_name} does not exist.")
+
+
+################################################   Task Description   ################################################
+# 4. Phonebook
+# Write a program that receives info from the console about people and their phone numbers.
+# Each entry should have a name and a number (both strings) separated by a "-".
+# If you receive a name that already exists in the phonebook, update its number.
+# After filling the phonebook, you will receive a number – N.
+# Your program should be able to perform a search of contact by name and print its details in the format:
+# "{name} -> {number}".
+# In case the contact isn't found, print: "Contact {name} does not exist."
