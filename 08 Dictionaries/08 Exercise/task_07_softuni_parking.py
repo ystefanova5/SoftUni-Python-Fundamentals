55b7508139e7e2ad535a79dc307791c9972c347a
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-16 22:09:39 +0200

    Update task_07_softuni_parking.py

--- 08 Dictionaries/08 Exercise/task_07_softuni_parking.py
+++ 08 Dictionaries/08 Exercise/task_07_softuni_parking.py
@@ -1,29 +1,3 @@
-# 7. SoftUni Parking
-# Task Description:
-# Write a program, which validates a parking place - users can register to enter the park and unregister to leave.
-# The program receives 2 types of commands:
-# • "register {username} {license_plate_number}":
-#   o The system only supports one car per user at the moment,
-# so if a user tries to register another license plate using the same username, the system should print:
-# "ERROR: already registered with plate number {license_plate_number}"
-#   o If the check above passes successfully, the user should be registered, so the system should print:
-#  "{username} registered {license_plate_number} successfully"
-# • "unregister {username}":
-#   o If the user is not present in the database, the system should print:
-# "ERROR: user {username} not found"
-#   o If the check above passes successfully, the system should print:
-# "{username} unregistered successfully"
-# After you execute all of the commands,
-# print all the currently registered users and their license plates in the format:
-# • "{username} => {license_plate_number}"
-# Input
-# • First line: n - number of commands - integer
-# • Next n lines: commands in one of the two possible formats:
-#   o Register: "register {username} {license_plate_number}"
-#   o Unregister: "unregister {username}"
-# The input will always be valid, and you do not need to check it explicitly.
-
-
 def register(username, license_plate_number, users):
     if username in users:
         return f"ERROR: already registered with plate number {license_plate_number}"
@@ -53,3 +27,30 @@ for command in range(number_of_commands):
         print(unregister(current_username, parking_users))
 for user in parking_users:
     print(f"{user} => {parking_users[user]}")
+
+
+################################################   Task Description   ################################################
+# 7. SoftUni Parking
+# Task Description:
+# Write a program, which validates a parking place - users can register to enter the park and unregister to leave.
+# The program receives 2 types of commands:
+# • "register {username} {license_plate_number}":
+#   o The system only supports one car per user at the moment,
+#   so if a user tries to register another license plate using the same username, the system should print:
+# "ERROR: already registered with plate number {license_plate_number}"
+#   o If the check above passes successfully, the user should be registered, so the system should print:
+#   "{username} registered {license_plate_number} successfully"
+# • "unregister {username}":
+#   o If the user is not present in the database, the system should print:
+# "ERROR: user {username} not found"
+#   o If the check above passes successfully, the system should print:
+# "{username} unregistered successfully"
+# After you execute all of the commands,
+# print all the currently registered users and their license plates in the format:
+# • "{username} => {license_plate_number}"
+# Input
+# • First line: n - number of commands - integer
+# • Next n lines: commands in one of the two possible formats:
+#   o Register: "register {username} {license_plate_number}"
+#   o Unregister: "unregister {username}"
+# The input will always be valid, and you do not need to check it explicitly.
