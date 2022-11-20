cd11044946864321c4d2c21dfc4f63397284f8b1
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-20 17:11:03 +0200

    Update task_07_patterns.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Labs/task_07_patterns.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Labs/task_07_patterns.py
@@ -1,12 +1,13 @@
-# 07. Patterns
-# Task Description:
-# Write a program that receives a number and creates the following pattern. 
-# The number represents the largest count of stars on one row.
-
-
 max_stars = int(input())
 for first_stars in range(max_stars):
     print("*" * (first_stars + 1))
 
 for second_stars in range(max_stars - 1, -1, -1):
     print("*" * (second_stars))
+
+
+################################################   Task Description   ################################################
+# 7. Patterns
+# Task Description:
+# Write a program that receives a number and creates the following pattern. 
+# The number represents the largest count of stars on one row.
