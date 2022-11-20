546cb40de9a18daf74c486df33628363217659de
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-20 17:08:11 +0200

    Update task_02_largest_of_three_numbers.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Labs/task_02_largest_of_three_numbers.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Labs/task_02_largest_of_three_numbers.py
@@ -1,10 +1,11 @@
-# 02. Largest of Three Numbers
-# Task Description:
-# Write a program that receives three whole numbers and prints the largest one.
-
-
 number_one = int(input())
 number_two = int(input())
 number_three = int(input())
 largest = max(number_one, number_two, number_three)
 print(largest)
+
+
+################################################   Task Description   ################################################
+# 2. Largest of Three Numbers
+# Task Description:
+# Write a program that receives three whole numbers and prints the largest one.
