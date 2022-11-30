2a487274a309aa87babb496aa294a546537a0ba2
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-30 10:01:20 +0200

    Update task_02_drink_something.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_02_drink_something.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_02_drink_something.py
@@ -10,3 +10,14 @@ else:
     drink = "whisky"
 print(f"drink {drink}")
 
+
+################################################   Task Description   ################################################
+# 2. Drink Something
+# Kids drink toddy, teens drink coke, young adults drink beer, and adults drink whisky. 
+# Create a program that receives a person's age and prints what he/she drinks.
+# Rules:
+# A kid is defined as someone under or at the age of 14.
+# A teen is defined as someone under or at the age of 18.
+# A young adult is defined as someone under or at the age of 21.
+# An adult is defined as someone above the age of 21.
+# Note: All the values are inclusive except the last one!
