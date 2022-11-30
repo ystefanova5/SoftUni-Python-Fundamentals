46c0425b6cb8b38a685377ffd50ecdbb17b1317d
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-30 10:00:19 +0200

    Update task_01_jennys_secret_message.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_01_jennys_secret_message.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_01_jennys_secret_message.py
@@ -3,3 +3,10 @@ if name != "Johnny":
     print(f"Hello, {name}!")
 else:
     print("Hello, my love!")
+
+
+################################################   Task Description   ################################################
+# 1. Jenny's Secret Message
+# Jenny studies programming with Python and wants to create a program that greets the user when he/she gives his/her name. 
+# The greeting should be in the format "Hello, {name}!". 
+# However, Jenny is in love with Johnny and would like to greet him differently: "Hello, my love!". Could you help her?
