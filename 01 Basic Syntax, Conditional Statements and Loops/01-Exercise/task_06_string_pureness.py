62f4eadc629188a995dd993ccd45352073bb90a7
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-30 10:06:05 +0200

    Update task_06_string_pureness.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_06_string_pureness.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_06_string_pureness.py
@@ -7,3 +7,12 @@ for _ in range(number_of_strings):
             break
     else:
         print(f"{current_string} is pure.")
+
+
+################################################   Task Description   ################################################
+# 6. String Pureness
+# You will be given number n. After that, you'll receive different strings n times. 
+# Your task is to check if the given strings are pure, meaning that they do NOT consist of any of the characters: 
+# comma ",", period ".", or underscore "_":
+# • If a string is pure, print "{string} is pure."
+# • Otherwise, print "{string} is not pure!"
