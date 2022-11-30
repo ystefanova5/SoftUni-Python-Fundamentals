5da05e8873274297ebd20100ad402e1085575805
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-30 10:04:42 +0200

    Update task_05_orders.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_05_orders.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_05_orders.py
@@ -9,3 +9,21 @@ for order in range(number_of_orders):
         total_sum += coffee_price
         print(f"The price for the coffee is: ${coffee_price:.2f}")
 print(f"Total: ${total_sum:.2f}")
+
+
+################################################   Task Description   ################################################
+# 5. Orders
+# You work at a coffee shop, and your job is to place orders to the distributors. 
+# Thus, you want to know the price of each order. 
+# On the first line, you will receive integer N - the number of orders the shop will receive. 
+# For each order, you will receive the following information:
+# • Price per capsule - a floating-point number in the range [0.01…100.00]
+# • Days - integer in the range [1…31]
+# • Capsules, needed per day - integer in the range [1…2000]
+# For each order, you should print a single line in the following format:
+# • "The price for the coffee is: ${price}"
+# If you do not receive a correct order (one or more of the values are not in the given range), 
+# you should ignore it and move to the next one.
+# After you go through all orders, you need to print the total price in the following format:
+# • "Total: ${total_price}"
+# Both the price of a coffee and the total price must be formatted to the second decimal place. 
