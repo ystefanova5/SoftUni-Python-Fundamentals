56f0c73a6c39947d02785d38fe22f0bf638187ae
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-30 10:03:34 +0200

    Update task_04_maximum_multiple.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_04_maximum_multiple.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_04_maximum_multiple.py
@@ -7,3 +7,14 @@ for number in range(1, boundary + 1):
     else:
         continue
 print(largest_possible_num)
+
+
+################################################   Task Description   ################################################
+# 4. Maximum Multiple
+# On the first line, you will be given a positive number, which will serve as a divisor. 
+# On the second line, you will receive a positive number that will be the boundary. 
+# You should find the largest integer N, that is:
+# • divisible by the given divisor
+# • less than or equal to the given bound
+# • greater than 0
+# Note: it is guaranteed that N is found.
