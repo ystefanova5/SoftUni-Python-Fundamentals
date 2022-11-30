452e58d39b97238b2c489b120a90facd43f385d3
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-11-30 10:09:37 +0200

    Update task_09_sorting_hat.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_09_sorting_hat.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-Exercise/task_09_sorting_hat.py
@@ -19,3 +19,21 @@ while command != "Welcome!":
     command = input()
 if hats_are_sorted:
     print("Welcome to Hogwarts.")
+
+
+################################################   Task Description   ################################################
+# 9. Sorting Hat
+# Help out the sorting hat to sort the new students in the houses of Hogwarts. 
+# You will be receiving names until the command "Welcome!". 
+# The length of each name determines in which house the student is going:
+# • If the name is less than 5 chars, the student is going into Gryffindor
+#     o Print "{name} goes to Gryffindor."
+# • If the name is exactly 5 chars, the student is going into Slytherin
+#     o Print "{name} goes to Slytherin."
+# • If the name is exactly 6 chars, the student is going into Ravenclaw
+#     o Print "{name} goes to Ravenclaw."
+# • If the name is more than 6 chars, the student is going into Hufflepuff
+#     o Print "{name} goes to Hufflepuff."
+# While receiving names, if you receive "Voldemort", 
+# print "You must not speak of that name!" and end the program. No more sorting for today!
+# If all students are sorted successfully, print "Welcome to Hogwarts."
