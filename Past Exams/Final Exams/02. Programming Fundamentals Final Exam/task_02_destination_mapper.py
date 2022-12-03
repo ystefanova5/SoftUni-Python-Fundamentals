3b1ebada140b3d573f2c0a70ba17ed90a66f457b
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-03 12:05:04 +0200

    Update task_02_destination_mapper.py

--- Past Exams/Final Exams/02. Programming Fundamentals Final Exam/task_02_destination_mapper.py
+++ Past Exams/Final Exams/02. Programming Fundamentals Final Exam/task_02_destination_mapper.py
@@ -19,8 +19,8 @@ print(f"Travel Points: {travel_points}")
 # Your next task is to mark all the points on the map that you are going to visit.
 # You will be given a string representing some places on the map. You have to filter only the valid ones. 
 # A valid location is:
-# •	Surrounded by "=" or "/" on both sides (the first and the last symbols must match)
-# •	After the first "=" or "/" there should be only letters 
+# • Surrounded by "=" or "/" on both sides (the first and the last symbols must match)
+# • After the first "=" or "/" there should be only letters 
 #   (the first must be upper-case, other letters could be upper or lower-case)
 # • The letters must be at least 3
 # Example: In the string "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=" only the first two locations are valid.
@@ -29,7 +29,7 @@ print(f"Travel Points: {travel_points}")
 # In the end, on the first line, print: "Destinations: {destinations joined by ', '}". 
 # On the second line, print "Travel Points: {travel_points}".
 # Input / Constraints
-# •	You will receive a string representing the locations on the map
-# •	JavaScript: you will receive a single parameter: string
+# • You will receive a string representing the locations on the map
+# • JavaScript: you will receive a single parameter: string
 # Output
-# •	Print the messages described above
+# • Print the messages described above
