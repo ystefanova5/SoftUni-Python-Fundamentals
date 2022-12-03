230abb15b49c48dd810abe05551aa30f2dda84c6
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-03 12:22:20 +0200

    Update task_02_fancy_barcodes.py

--- Past Exams/Final Exams/04. Programming Fundamentals Final Exam/task_02_fancy_barcodes.py
+++ Past Exams/Final Exams/04. Programming Fundamentals Final Exam/task_02_fancy_barcodes.py
@@ -16,3 +16,36 @@ for _ in range(count):
         print(f"Product group: {product_group}")
     else:
         print("Invalid barcode")
+
+
+################################################   Task Description   ################################################
+# Problem 2 - Fancy Barcodes
+# 
+# Problem for exam preparation for the Programming Fundamentals Course @SoftUni.
+# Submit your solutions in the SoftUni judge system at https://judge.softuni.org/Contests/Practice/Index/2303#1.
+# 
+# Your first task is to determine if the given sequence of characters is a valid barcode or not. 
+# Each line must not contain anything else but a valid barcode. A barcode is valid when:
+#     • It is surrounded by a "@" followed by one or more "#"
+#     • It is at least 6 characters long (without the surrounding "@" or "#")
+#     • It starts with a capital letter
+#     • It contains only letters (lower and upper case) and digits
+#     • It ends with a capital letter
+# Examples of valid barcodes: @###Che46sE@##, @#FreshFisH@#, @###Brea0D@###, @##Che46sE@##
+# Examples of invalid barcodes: ##InvaliDiteM##, @InvalidIteM@, @#Invalid_IteM@#
+# Next, you have to determine the product group of the item from the barcode. 
+# The product group is obtained by concatenating all the digits found in the barcode. 
+# If there are no digits present in the barcode, the default product group is "00".
+# Examples:  
+# @#FreshFisH@# -> product group: 00
+# @###Brea0D@### -> product group: 0
+# @##Che4s6E@## -> product group: 46
+# Input
+# On the first line, you will be given an integer n – the count of barcodes that you will be receiving next. 
+# On the following n lines, you will receive different strings.
+# Output
+# For each barcode that you process, you need to print a message.
+# If the barcode is invalid:
+#     • "Invalid barcode"
+# If the barcode is valid:
+#     • "Product group: {product group}"
