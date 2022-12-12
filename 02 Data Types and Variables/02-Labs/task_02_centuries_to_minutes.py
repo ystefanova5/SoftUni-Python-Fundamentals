dc6540768773b8e6f5d823f2752c2fe242409646
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-12 16:41:01 +0200

    Update task_02_centuries_to_minutes.py

--- 02 Data Types and Variables/02-Labs/task_02_centuries_to_minutes.py
+++ 02 Data Types and Variables/02-Labs/task_02_centuries_to_minutes.py
@@ -5,3 +5,8 @@ days = int(years * days_in_one_year)
 hours = days * 24
 minutes = hours * 60
 print(f"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
+
+
+################################################   Task Description   ################################################
+# 2. Centuries to Minutes
+# Write a program that reads an integer number of centuries and converts it to years, days, hours, and minutes.
