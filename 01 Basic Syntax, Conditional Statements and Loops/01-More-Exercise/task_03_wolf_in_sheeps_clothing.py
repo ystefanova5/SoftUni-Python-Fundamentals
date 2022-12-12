d918fe2ad034ceddcff713053bbc80aab1527987
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-12 16:37:46 +0200

    Update task_03_wolf_in_sheeps_clothing.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-More-Exercise/task_03_wolf_in_sheeps_clothing.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-More-Exercise/task_03_wolf_in_sheeps_clothing.py
@@ -10,3 +10,20 @@ else:
         if current_animal == "wolf":
             sheep_in_danger = index
             print(f"Oi! Sheep number {sheep_in_danger}! You are about to be eaten by a wolf!")
+
+
+################################################   Task Description   ################################################
+# 3. Wolf in Sheep's Clothing
+# Wolves have been reintroduced to Great Britain. 
+# You are a sheep farmer and are now plagued by wolves who pretend to be sheep. 
+# Fortunately, you are good at spotting them.
+# Warn the sheep in front of the wolf that it is about to be eaten. 
+# Remember that you are standing at the front of the queue, which is at the end of the list:
+# [sheep, sheep, wolf, sheep, sheep] (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+#    4      3            2      1
+# If the wolf is the closest animal to you, print "Please go away and stop eating my sheep". 
+# Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" 
+# where N is the sheep's position in the queue.
+# Note: there will always be exactly one wolf on the list.
+# Input
+# The input will be a single string containing the animals separated by a comma and a single space ", "
