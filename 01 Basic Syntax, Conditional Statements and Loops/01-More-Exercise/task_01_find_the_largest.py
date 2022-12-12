65d26657b79c195bbd84460c929ab36624d54da0
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-12 16:35:45 +0200

    Update task_01_find_the_largest.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-More-Exercise/task_01_find_the_largest.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-More-Exercise/task_01_find_the_largest.py
@@ -5,3 +5,8 @@ final_str = ""
 for i in range(len(number_str)):
     final_str += sorted_num[i]
 print(int(final_str))
+
+
+################################################   Task Description   ################################################
+# 1. Find the Largest
+# You will be given a number. Print the largest number that can be formed from the digits of the given number.
