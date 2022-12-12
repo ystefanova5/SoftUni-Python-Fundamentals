d5a85a26cb0560b4dd5507ed94d912ecc9bf574e
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-12 16:36:22 +0200

    Update task_02_find_the_capitals.py

--- 01 Basic Syntax, Conditional Statements and Loops/01-More-Exercise/task_02_find_the_capitals.py
+++ 01 Basic Syntax, Conditional Statements and Loops/01-More-Exercise/task_02_find_the_capitals.py
@@ -5,3 +5,8 @@ for i in range(len(initial_string)):
     if 65 <= ord(current_letter) <= 90:
         final_list.append(i)
 print(final_list)
+
+
+################################################   Task Description   ################################################
+# 2. Find the Capitals
+# Write a program that takes a single string and prints a list of all the capital letters indices.
