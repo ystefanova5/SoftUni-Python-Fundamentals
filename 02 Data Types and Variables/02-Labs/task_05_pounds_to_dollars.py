e83e16afa3410da338bf5d611f5f37b863c5225e
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-12 16:44:16 +0200

    Update and rename task_5_pounds_to_dollars.py to task_05_pounds_to_dollars.py

---
+++ 02 Data Types and Variables/02-Labs/task_05_pounds_to_dollars.py
@@ -0,0 +1,10 @@
+british_pounds = int(input())
+conversion_rate = 1.31
+dollars = british_pounds * conversion_rate
+print(f"{dollars:.3f}")
+
+
+################################################   Task Description   ################################################
+# 5. Pounds to Dollars
+# Write a program that converts British pounds (integer) to US dollars formatted to the 3rd decimal point.
+# 1 British Pound = 1.31 Dollars.
--- 02 Data Types and Variables/02-Labs/task_5_pounds_to_dollars.py
+++
@@ -1,4 +0,0 @@
-british_pounds = int(input())
-conversion_rate = 1.31
-dollars = british_pounds * conversion_rate
-print(f"{dollars:.3f}")
