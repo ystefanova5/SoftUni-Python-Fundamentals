d57c8317235a0d3deba505f7d7b6908c3c83bacc
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:39:51 +0200

    Update task_03_elevator.py

--- 02 Data Types and Variables/02-Exercise/task_03_elevator.py
+++ 02 Data Types and Variables/02-Exercise/task_03_elevator.py
@@ -9,4 +9,7 @@ else:
 print(courses)
 
 
-
+################################################   Task Description   ################################################
+# 3. Elevator
+# Calculate how many courses will be needed to elevate N persons by using an elevator with a capacity of P persons. 
+# The input holds two lines: the number of people N and the capacity P of the elevator.
