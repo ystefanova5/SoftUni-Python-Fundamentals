594d81e1743ec0c9da51682c32ce9c68af67ccd2
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:40:46 +0200

    Update task_04_sum_of_chars.py

--- 02 Data Types and Variables/02-Exercise/task_04_sum_of_chars.py
+++ 02 Data Types and Variables/02-Exercise/task_04_sum_of_chars.py
@@ -5,3 +5,11 @@ for i in range(number_of_characters):
     sum_of_characters += ord(current_character)
 print(f"The sum equals: {sum_of_characters}")
 
+
+################################################   Task Description   ################################################
+# 4. Sum of Chars
+# Write a program, which sums the ASCII codes of N characters and prints the sum on the console. 
+# On the first line, you will receive N – the number of lines. 
+# On the following N lines – you will receive a letter per line. 
+# Print the total sum in the following format: "The sum equals: {total_sum}".
+# Note: n will be in the interval [1…20].
