4c769999d6d0cddbbe3a4e743ffee0604656f9b5
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:38:39 +0200

    Update task_01_integer_operations.py

--- 02 Data Types and Variables/02-Exercise/task_01_integer_operations.py
+++ 02 Data Types and Variables/02-Exercise/task_01_integer_operations.py
@@ -4,3 +4,10 @@ third_number = int(input())
 fourth_number = int(input())
 result = int((first_number + second_number) / third_number) * fourth_number
 print(result)
+
+
+################################################   Task Description   ################################################
+# 1. Integer Operations
+# Write a program that reads four integer numbers. It should add the first to the second number, 
+# integer divide the sum by the third number, and multiply the result by the fourth number. 
+# Print the final result.
