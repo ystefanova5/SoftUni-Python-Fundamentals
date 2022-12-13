cfdac4ef463c2b44f42c3eb4bb36289bb12f1721
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:41:23 +0200

    Update task_05_print_part_of_the_ascii_table.py

--- 02 Data Types and Variables/02-Exercise/task_05_print_part_of_the_ascii_table.py
+++ 02 Data Types and Variables/02-Exercise/task_05_print_part_of_the_ascii_table.py
@@ -3,3 +3,10 @@ end_index = int(input())
 for character in range(start_index, end_index + 1):
     current_character = chr(character)
     print(current_character, end=" ")
+
+
+################################################   Task Description   ################################################
+# 5. Print Part of the ASCII Table
+# Write a program that prints part of the ASCII table characters on the console, separated by a single space. 
+# On the first line of input, you will receive the char index you should start with. 
+# On the second line - the index of the last character you should print. 
