42aae7aa79d2407df61d358b74ee19478f1f16fc
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:42:07 +0200

    Update task_06_triples_of_latin_letters.py

--- 02 Data Types and Variables/02-Exercise/task_06_triples_of_latin_letters.py
+++ 02 Data Types and Variables/02-Exercise/task_06_triples_of_latin_letters.py
@@ -3,3 +3,8 @@ for first_letter in range(number_of_letters):
     for second_letter in range(number_of_letters):
         for third_letter in range(number_of_letters):
             print(f"{chr(97 + first_letter)}{chr(97 + second_letter)}{chr(97 + third_letter)}")
+
+
+################################################   Task Description   ################################################
+# 6. Triples of Latin Letters
+# Write a program to read an integer N and print all triples of the first N small Latin letters, ordered alphabetically.
