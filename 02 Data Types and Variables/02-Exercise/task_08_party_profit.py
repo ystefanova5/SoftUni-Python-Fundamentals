8efb78787181ab156875ead23ddc7e795ff0c3c8
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:43:52 +0200

    Update task_08_party_profit.py

--- 02 Data Types and Variables/02-Exercise/task_08_party_profit.py
+++ 02 Data Types and Variables/02-Exercise/task_08_party_profit.py
@@ -17,3 +17,23 @@ for day in range(1, days + 1):
 coins = int(total_coins / group_size)
 print(f"{group_size} companions received {coins} coins each.")
 
+
+################################################   Task Description   ################################################
+# 8. * Party Profit
+# As a young adventurer, you travel with your group worldwide, seeking for gold and glory. 
+# But you need to split the profit among your companions.
+# You will receive a group size. After that, you receive the days of the adventure. 
+# Every day, you earn 50 coins, but you also spend 2 coins per companion for food. 
+# Every 3rd (third) day, you organize a motivational party, spending 3 coins per companion for drinking water. 
+# Every 5th (fifth) day, you slay a boss monster and gain 20 coins per companion. 
+# But if you have a motivational party the same day, you spend additional 2 coins per companion. 
+# Every 10th (tenth) day at the start of the day, 2 (two) of your companions leave, 
+# but every 15th (fifteenth) day 5 (five) new companions are joined at the beginning of the day.
+# You should calculate how many coins gets each companion at the end of the adventure.
+# Input / Constraints
+# The input will consist of exactly 2 lines:
+#     • group size – integer in the range [1…100]
+#     • days – integer in the range [1…100]
+# Output
+# Print the following message: "{companions_count} companions received {coins} coins each."
+# Note: You cannot split a coin, so you should round down the coins to an integer number.
