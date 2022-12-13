3b8d01420de1e61c729122a415036b1cbbd320b3
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:46:40 +0200

    Update task_10_gladiator_expenses.py

--- 02 Data Types and Variables/02-Exercise/task_10_gladiator_expenses.py
+++ 02 Data Types and Variables/02-Exercise/task_10_gladiator_expenses.py
@@ -17,3 +17,27 @@ for fight in range(1, lost_fights_count + 1):
         expenses += armor_price
         shield_count = 0
 print(f"Gladiator expenses: {expenses:.2f} aureus")
+
+
+################################################   Task Description   ################################################
+# 
+# 10. * Gladiator Expenses
+# 
+# As a gladiator, Peter needs to repair his broken equipment when he loses a fight. 
+# His equipment consists of a helmet, a sword, a shield, and an armor. 
+# You will receive Peter's lost fights count. 
+# Every second lost game, his helmet is broken.
+# Every third lost game, his sword is broken.
+# When both his sword and helmet are broken in the same lost fight, his shield also breaks.
+# Every second time his shield brakes, his armor also needs to be repaired. 
+# You will receive the price of each item in his equipment. 
+# Calculate his expenses for the year for renewing his equipment. 
+# Input / Constraints
+# The input will consist of 5 lines:
+# • On the first line, you will receive the lost fights count – an integer in the range [0, 1000].
+# • On the second line, you will receive the helmet price - a floating-point number in the range [0, 1000]. 
+# • On the third line, you will receive the sword price - a floating-point number in the range [0, 1000]. 
+# • On the fourth line, you will receive the shield price - a floating-point number in the range [0, 1000]. 
+# • On the fifth line, you will receive the armor price - a floating-point number in the range [0, 1000]. 
+# Output
+# • As output, you must print Peter`s total expenses for new equipment: "Gladiator expenses: {expenses} aureus"
