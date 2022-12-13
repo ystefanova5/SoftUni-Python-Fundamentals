4373593a680732a1a56fd3f41bc9220f7d484917
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:39:11 +0200

    Update task_02_chars_to_string.py

--- 02 Data Types and Variables/02-Exercise/task_02_chars_to_string.py
+++ 02 Data Types and Variables/02-Exercise/task_02_chars_to_string.py
@@ -2,3 +2,9 @@ first_character = input()
 second_character = input()
 third_character = input()
 print(f"{first_character}{second_character}{third_character}")
+
+
+################################################   Task Description   ################################################
+# 2. Chars to String
+# Write a function that receives 3 characters. 
+# Concatenate all the characters into one string and print it on the console.
