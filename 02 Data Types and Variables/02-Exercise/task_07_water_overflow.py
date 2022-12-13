59ed5524b14fd44e3cabb95c820b708de7d1ef6a
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:42:57 +0200

    Update task_07_water_overflow.py

--- 02 Data Types and Variables/02-Exercise/task_07_water_overflow.py
+++ 02 Data Types and Variables/02-Exercise/task_07_water_overflow.py
@@ -9,3 +9,11 @@ for lines in range(number_of_lines):
         filled_capacity += current_liters
 print(filled_capacity)
 
+
+################################################   Task Description   ################################################
+# 7. Water Overflow
+# You have a water tank with a capacity of 255 liters. 
+# On the first line, you will receive n – the number of lines, which will follow. 
+# On the following n lines, you will receive liters of water (integers), which you should pour into your tank. 
+# If the capacity is not enough, print "Insufficient capacity!" and continue reading the next line. 
+# On the last line, print the liters in the tank.
