0f05c96afbaac8f9848a905c101056ab93d82ca1
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-13 15:45:15 +0200

    Update task_09_snowballs.py

--- 02 Data Types and Variables/02-Exercise/task_09_snowballs.py
+++ 02 Data Types and Variables/02-Exercise/task_09_snowballs.py
@@ -14,3 +14,30 @@ for snowball in range(number_of_snowballs):
         highest_snowball_time = current_snowball_time
         highest_snowball_quality = current_snowball_quality
 print(f"{highest_snowball_weight} : {highest_snowball_time} = {highest_snowball_value} ({highest_snowball_quality})")
+
+
+################################################   Task Description   ################################################
+# 9. *Snowballs
+# Tony and Andi love playing in the snow and having snowball fights, 
+# but they always argue about who makes the best snowballs. 
+# They have decided to involve you in their fray by writing a program 
+# that calculates snowball data and outputs the best snowball value.
+# You will receive N – an integer, the number of snowballs being made by Tony and Andi.
+# On the following lines, you will receive 3 inputs for each snowball:
+#     • The weight of the snowball (integer).
+#     • The time needed for the snowball to get to its target (integer).
+#     • The quality of the snowball (integer).
+# For each snowball, you must calculate its value by the following formula:
+# (snowball_weight / snowball_time) ** snowball_quality
+# In the end, you must print the highest calculated value of a snowball.
+# Input
+#     • On the first input line, you will receive N – the number of snowballs.
+#     • On the next N*3 input lines, you will be receiving data about each snowball. 
+# Output
+#     • You need to print the highest calculated snowball's value in the format: 
+#     "{snowball_weight} : {snowball_time} = {snowball_value} ({snowball_quality})"
+# Constraints
+#     • The number of snowballs (N) will be an integer in range [0, 100].
+#     • The weight is an integer in the range [0, 1000].
+#     • The time is an integer in the range [1, 500].
+#     • The quality is an integer in the range [0, 100].
