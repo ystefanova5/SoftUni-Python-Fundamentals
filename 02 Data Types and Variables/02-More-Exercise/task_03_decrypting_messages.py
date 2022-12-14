b75b8371a744c454fb4d84e1a383c45c160bf264
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-14 10:59:18 +0200

    Update task_03_decrypting_messages.py

--- 02 Data Types and Variables/02-More-Exercise/task_03_decrypting_messages.py
+++ 02 Data Types and Variables/02-More-Exercise/task_03_decrypting_messages.py
@@ -4,3 +4,12 @@ for _ in range(number_of_lines):
     input_letter = input()
     output_letter = chr(ord(input_letter) + key)
     print(output_letter, end="")
+
+
+################################################   Task Description   ################################################
+# 3. Decrypting Messages
+# On the first line, you will receive a key (integer).
+# On the second line, you will receive n – the number of lines, which will follow. 
+# On the next n lines – you will receive a lower and an uppercase letter per line.
+# You should add the key to each of the characters and append them to a message. 
+# In the end, print the decrypted message. 
