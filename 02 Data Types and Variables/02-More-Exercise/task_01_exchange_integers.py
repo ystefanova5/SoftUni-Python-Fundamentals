1a263a18dd09e47254a95ac69c5931f489c8be2c
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-14 10:58:06 +0200

    Update task_01_exchange_integers.py

--- 02 Data Types and Variables/02-More-Exercise/task_01_exchange_integers.py
+++ 02 Data Types and Variables/02-More-Exercise/task_01_exchange_integers.py
@@ -7,3 +7,9 @@ first_number, second_number = second_number, first_number
 print("After:")
 print(f"a = {first_number}")
 print(f"b = {second_number}")
+
+
+################################################   Task Description   ################################################
+# 1. Exchange Integers
+# Read two integer numbers and, after that, exchange their values. 
+# Print the variable values before and after the exchange.
