4b9a4a696cbcfde53d542db628d8d2a2d78aaff1
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-15 20:57:12 +0200

    Update task_04_search.py

--- 03 Lists Basics/03-Labs/task_04_search.py
+++ 03 Lists Basics/03-Labs/task_04_search.py
@@ -9,3 +9,10 @@ for _ in range(number):
         filtered_list.append(input_string)
 print(full_list)
 print(filtered_list)
+
+
+################################################   Task Description   ################################################
+# 4. Search
+# On the first line, you will receive a number n. On the second line, you will receive a word. 
+# On the following n lines, you will be given some strings. You should add them to a list and print them. 
+# After that, you should filter out only the strings that include the given word and print that list too.
