e6b3aba079ebe41dc753e2670097fbad5b87a910
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-15 20:54:45 +0200

    Update task_01_strange_zoo.py

--- 03 Lists Basics/03-Labs/task_01_strange_zoo.py
+++ 03 Lists Basics/03-Labs/task_01_strange_zoo.py
@@ -3,3 +3,14 @@ body = input()
 head = input()
 new_list = [head, body, tail]
 print(new_list)
+
+
+################################################   Task Description   ################################################
+# 1. Strange Zoo
+# You are at the zoo, and the meerkats look strange. 
+# You will receive 3 strings on separate lines, representing the tail, 
+# the body, and the head of an animal in that order. 
+# Your task is to re-arrange the elements in a list so that the animal looks normal again:
+#   •	On the first position is the head;
+#   •	On the second position is the body;
+#   •	On the last one is the tail.
