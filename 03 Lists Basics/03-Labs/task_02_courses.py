04a72812d548e825f1b2016324abdc1c1922b4a8
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-15 20:55:30 +0200

    Update task_02_courses.py

--- 03 Lists Basics/03-Labs/task_02_courses.py
+++ 03 Lists Basics/03-Labs/task_02_courses.py
@@ -4,3 +4,9 @@ for items in range(number):
     current_course = input()
     list_of_courses.append(current_course)
 print(list_of_courses)
+
+
+################################################   Task Description   ################################################
+# 2. Courses
+# On the first line, you will receive a single number n. 
+# On the following n lines, you will receive names of courses. You should create a list of courses and print it.
