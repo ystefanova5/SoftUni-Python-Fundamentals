4ebf983f7a4d3367f56f59c3af57ca6b5b37ee0d
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-15 20:59:08 +0200

    Update task_05_numbers_filter.py

--- 03 Lists Basics/03-Labs/task_05_numbers_filter.py
+++ 03 Lists Basics/03-Labs/task_05_numbers_filter.py
@@ -22,3 +22,14 @@ elif command == "positive":
         if n >= 0:
             filtered_list.append(n)
 print(filtered_list)
+
+
+################################################   Task Description   ################################################
+# 5. Numbers Filter
+# On the first line, you will receive a single number n. On the following n lines, you will receive integers. 
+# After that, you will be given one of the following commands:
+#     • even
+#     • odd
+#     • negative
+#     • positive
+# Filter all the numbers that fit in the category (0 counts as a positive and even). Finally, print the result.
