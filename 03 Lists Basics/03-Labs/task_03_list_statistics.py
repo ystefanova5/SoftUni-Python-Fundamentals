c4865efd769da2d5bee4b39d8dfa9b9db31c4cc2
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-15 20:56:24 +0200

    Update task_03_list_statistics.py

--- 03 Lists Basics/03-Labs/task_03_list_statistics.py
+++ 03 Lists Basics/03-Labs/task_03_list_statistics.py
@@ -11,3 +11,14 @@ print(positives_list)
 print(negatives_list)
 print(f"Count of positives: {len(positives_list)}")
 print(f"Sum of negatives: {sum(negatives_list)}")
+
+
+################################################   Task Description   ################################################
+# 3. List Statistics
+# On the first line, you will receive a number n. On the following n lines, you will receive integers. 
+# You should create and print two lists:
+#     • One with all the positives (including 0) numbers
+#     • One with all the negatives numbers
+# Finally, print the following message: 
+# "Count of positives: {count_positives}
+# Sum of negatives: {sum_of_negatives}"
