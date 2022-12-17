fd3eb690c6a022758ee192e7c28f80c82ecba0bb
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-17 14:36:56 +0200

    Update task_06_survival_of_the_biggest.py

--- 03 Lists Basics/03-Exercise/task_06_survival_of_the_biggest.py
+++ 03 Lists Basics/03-Exercise/task_06_survival_of_the_biggest.py
@@ -9,3 +9,11 @@ for j in range(len(integers_list)):
         print(integers_list[j], end="")
     else:
         print(f", {integers_list[j]}", end="")
+
+
+################################################   Task Description   ################################################
+# 6. Survival of the Biggest
+# Write a program that receives a list of integer numbers (separated by a single space) and a number n. 
+# The number n represents the count of numbers to remove from the list. 
+# You should remove the smallest ones, and then, you should print all the numbers that are left in the list, 
+# separated by a comma and a space ", ".
