93075c2a9f698a52626ff64eb89ee3fc267c5bb4
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-17 14:59:11 +0200

    Update task_07_easter_gifts.py

--- 03 Lists Basics/03-Exercise/task_07_easter_gifts.py
+++ 03 Lists Basics/03-Exercise/task_07_easter_gifts.py
@@ -26,25 +26,25 @@ for g in range(len(gifts_list)):
 
 
 ################################################   Task Description   ################################################
-7. * Easter Gifts
-As a good friend, you decide to buy presents for your friends.
-Create a program that helps you plan the gifts for your friends and family. 
-First, you are going to receive the gifts you plan on buying on a single line, 
-separated by space, in the following format:
-"{gift1} {gift2} {gift3}… {giftn}"
-Then you will start receiving commands until you read the "No Money" message. There are three possible commands:
-    • "OutOfStock {gift}"
-        o Find the gifts with this name in your collection, if any, and change their values to "None".  
-    • "Required {gift} {index}"
-        o If the index is valid, replace the gift on the given index with the given gift. 
-    • "JustInCase {gift}"
-        o Replace the value of your last gift with this one. 
-In the end, print the gifts on a single line, except the ones with value "None", 
-separated by a single space in the following format:
-"{gift1} {gift2} {gift3} … {giftn}"
-Input / Constraints
-    • On the 1st line,  you will receive the names of the gifts, separated by a single space.
-    • On the following lines, until the "No Money" command is received, you will be receiving commands.
-    • The input will always be valid.
-Output
-    • Print the gifts in the format described above.
+# 7. * Easter Gifts
+# As a good friend, you decide to buy presents for your friends.
+# Create a program that helps you plan the gifts for your friends and family. 
+# First, you are going to receive the gifts you plan on buying on a single line, 
+# separated by space, in the following format:
+# "{gift1} {gift2} {gift3}… {giftn}"
+# Then you will start receiving commands until you read the "No Money" message. There are three possible commands:
+#     • "OutOfStock {gift}"
+#         o Find the gifts with this name in your collection, if any, and change their values to "None".  
+#     • "Required {gift} {index}"
+#         o If the index is valid, replace the gift on the given index with the given gift. 
+#     • "JustInCase {gift}"
+#         o Replace the value of your last gift with this one. 
+# In the end, print the gifts on a single line, except the ones with value "None", 
+# separated by a single space in the following format:
+# "{gift1} {gift2} {gift3} … {giftn}"
+# Input / Constraints
+#     • On the 1st line,  you will receive the names of the gifts, separated by a single space.
+#     • On the following lines, until the "No Money" command is received, you will be receiving commands.
+#     • The input will always be valid.
+# Output
+#     • Print the gifts in the format described above.
