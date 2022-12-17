d4b8536938873eeaa6f2bdb9654af60a28cd4160
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-17 14:31:46 +0200

    Update task_01_invert_values.py

--- 03 Lists Basics/03-Exercise/task_01_invert_values.py
+++ 03 Lists Basics/03-Exercise/task_01_invert_values.py
@@ -5,3 +5,9 @@ for i in range(len(first_list)):
     new_value = int(first_list[i]) * -1
     second_list.append(new_value)
 print(second_list)
+
+
+################################################   Task Description   ################################################
+# 1. Invert Values
+# Write a program that receives a single string containing positive and negative numbers separated by a single space. 
+# Print a list containing the opposite of each number.
