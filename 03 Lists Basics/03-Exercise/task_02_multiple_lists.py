621f9017ec74f570fe2f9a3b01a7ea01c5e65318
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-17 14:32:27 +0200

    Update task_02_multiple_lists.py

--- 03 Lists Basics/03-Exercise/task_02_multiple_lists.py
+++ 03 Lists Basics/03-Exercise/task_02_multiple_lists.py
@@ -6,3 +6,12 @@ for i in range(count - 1):
     current_number += factor
     output_list.append(current_number)
 print(output_list)
+
+
+################################################   Task Description   ################################################
+# 2. Multiples List
+# Write a program that receives two numbers (factor and count). 
+# It should create a list with a length of the given count that contains only integer numbers, 
+# which are multiples of the given factor. 
+# The numbers should be only positive, and they should be arranged in ascending order, 
+# starting from the value of the factor.
