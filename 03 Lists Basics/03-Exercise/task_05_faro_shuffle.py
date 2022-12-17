8b09a75f39d11a02d4165ecce8dc6c42629afbff
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-17 14:36:17 +0200

    Update task_05_faro_shuffle.py

--- 03 Lists Basics/03-Exercise/task_05_faro_shuffle.py
+++ 03 Lists Basics/03-Exercise/task_05_faro_shuffle.py
@@ -11,3 +11,16 @@ for shuffle in range(shuffles_count):
         shuffled_string.append(first_list[i])
         shuffled_string.append(second_list[i])
 print(shuffled_string)
+
+
+################################################   Task Description   ################################################
+# 5. Faro Shuffle
+# A faro shuffle is a method for shuffling a deck of cards, in which the deck is split exactly in half. 
+# Then the cards in the two halves are perfectly interleaved, 
+# such that the original bottom card is still on the bottom and the original top card is still on top.
+# For example, faro shuffling the list ['ace', 'two', 'three', 'four', 'five', 'six'] once, 
+# gives ['ace', 'four', 'two', 'five', 'three', 'six']
+# Write a program that receives a single string (cards separated by space) 
+# and on the second line receives a count of faro shuffles that should be made. 
+# Print the state of the deck after the shuffle.
+# Note: The length of the deck of cards will always be an even number.
