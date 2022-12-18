e3ec5469697d2897d6317f97cf4a975b3a8a9af1
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-18 11:49:44 +0200

    Update task_02_messaging.py

--- 03 Lists Basics/03-More-Exercise/task_02_messaging.py
+++ 03 Lists Basics/03-More-Exercise/task_02_messaging.py
@@ -19,3 +19,17 @@ for symbol in range(len(numbers_list)):
                    input_string[current_index + 1::]
     final_message += current_symbol
 print(final_message)
+
+
+################################################   Task Description   ################################################
+# 2. Messaging
+# On the first line, you will receive a sequence of numbers separated by a single space. 
+# On the second line, you will receive a string.
+# Your task is to write a program that sends a message only using chars from the given string. 
+# Each char the program adds to the message should be found by its index. 
+# The index you are looking for is the sum of a number's digits from the first sequence. 
+# If the index is greater than the length of the text, 
+# continue counting from the beginning (so that you always have a valid index). 
+# When you find a char, you should add it to the message and remove it from the string. 
+# It means that for the following index, the text will contain one character less.
+# Print the final message.
