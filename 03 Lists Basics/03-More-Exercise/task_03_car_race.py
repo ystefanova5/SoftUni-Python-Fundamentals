8a5f224942bcbeb5a982a71fbf54eed8203dadeb
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-18 11:52:58 +0200

    Update task_03_car_race.py

--- 03 Lists Basics/03-More-Exercise/task_03_car_race.py
+++ 03 Lists Basics/03-More-Exercise/task_03_car_race.py
@@ -24,3 +24,17 @@ else:
     winner = "right"
     winner_time = right_car_time
 print(f"The winner is {winner} with total time: {winner_time:.1f}")
+
+
+################################################   Task Description   ################################################
+# 3. Car Race
+# Write a program that announces the winner of a car race. 
+# You will receive a sequence of numbers. 
+# Each number represents the time the car needs to pass through that step (the index). 
+# There will be two cars. The first one starts from the left side, and the other one starts from the right side. 
+# The middle index of the sequence is the finish line. 
+# Calculate the total time each racer needs to reach the finish line 
+# and print the winner with his total time (the racer with less time). 
+# If you have a zero in the list, you should reduce the racer's time that reached it by 20% (from his current time).
+# The number of elements in the sequence will always be odd.
+# Print the result in the following format "The winner is {left/right} with total time: {total_time}".
