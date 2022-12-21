93d302c0aff43d26676a1b5d539aed18d475a21a
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:11:11 +0200

    Update task_10_perfect_number.py

--- 04 Functions/04-Exercise/task_10_perfect_number.py
+++ 04 Functions/04-Exercise/task_10_perfect_number.py
@@ -11,3 +11,13 @@ def find_perfect_number(number):
 
 current_number = int(input())
 print(find_perfect_number(current_number))
+
+
+################################################   Task Description   ################################################
+# 10. Perfect Number 
+# A perfect number is a positive integer that is equal to the sum of its proper positive divisors. 
+# That is the sum of its positive divisors, excluding the number itself (also known as its aliquot sum).
+# Write a function that receives an integer number and returns one of the following messages:
+#     • "We have a perfect number!" - if the number is perfect.
+#     • "It's not so perfect." - if the number is NOT perfect.
+# Print the result on the console.
