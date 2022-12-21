01abfee835f28ef4d1524c34bda91a23e72a3f54
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:07:04 +0200

    Update task_04_odd_and_even_sum.py

--- 04 Functions/04-Exercise/task_04_odd_and_even_sum.py
+++ 04 Functions/04-Exercise/task_04_odd_and_even_sum.py
@@ -19,3 +19,11 @@ def odd_even_sum(number):
 
 input_number = int(input())
 print(odd_even_sum(input_number))
+
+
+################################################   Task Description   ################################################
+# 4. Odd and Even Sum
+# You will receive a single number. You should write a function that returns the sum 
+# of all even and all odd digits in a given number. The result should be returned as a single string in the format: 
+# "Odd sum = {sum_of_odd_digits}, Even sum = {sum_of_even_digits}"
+# Print the result of the function on the console.
