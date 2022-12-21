edad15ee61b35ef961331480e84cafa8f955c520
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:03:55 +0200

    Update task_02_add_and_subtract.py

--- 04 Functions/04-Exercise/task_02_add_and_subtract.py
+++ 04 Functions/04-Exercise/task_02_add_and_subtract.py
@@ -15,3 +15,13 @@ first_number = int(input())
 second_number = int(input())
 third_number = int(input())
 print(add_and_subtract(first_number, second_number, third_number))
+
+
+################################################   Task Description   ################################################
+# 2. Add and Subtract
+# You will receive three integer numbers. 
+# Write functions named:
+#     • sum_numbers() that returns the sum of the first two integers
+#     • subtract() that returns the difference between the returned result of the first function and the third integer
+# Wrap the two functions in a function named add_and_subtract() which will receive the three numbers as parameters. 
+# Print the result of the subtract() function on the console.
