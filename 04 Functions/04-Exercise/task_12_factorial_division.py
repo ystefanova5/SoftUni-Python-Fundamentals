c0292f1e685ec983da1ddce80f5aa01395f4df00
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:12:32 +0200

    Update task_12_factorial_division.py

--- 04 Functions/04-Exercise/task_12_factorial_division.py
+++ 04 Functions/04-Exercise/task_12_factorial_division.py
@@ -9,3 +9,9 @@ first_number = int(input())
 second_number = int(input())
 result = calculate_factorial(first_number) / calculate_factorial(second_number)
 print(f"{result:.2f}")
+
+
+################################################   Task Description   ################################################
+# 12. * Factorial Division
+# Write a function that receives two integer numbers. Calculate the factorial of each number. 
+# Divide the first result by the second and print the division formatted to the second decimal point.
