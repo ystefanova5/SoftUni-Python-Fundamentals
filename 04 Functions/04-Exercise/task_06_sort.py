f1baf180e194afad0de03297bbf917b4182d7982
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:08:07 +0200

    Update task_06_sort.py

--- 04 Functions/04-Exercise/task_06_sort.py
+++ 04 Functions/04-Exercise/task_06_sort.py
@@ -1,3 +1,9 @@
 numbers_list = list(map(int, input().split(" ")))
 sorted_list = sorted(numbers_list)
 print(sorted_list)
+
+
+################################################   Task Description   ################################################
+# 6. Sort
+# Write a program that receives a sequence of numbers (integers) separated by a single space. 
+# It should print a sorted list of numbers in ascending order. Use sorted().
