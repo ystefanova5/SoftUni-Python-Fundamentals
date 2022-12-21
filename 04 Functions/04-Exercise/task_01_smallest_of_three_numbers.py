2c7a575b556977207c4bf0d6ddff8e3b1c7312c4
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:03:12 +0200

    Update task_01_smallest_of_three_numbers.py

--- 04 Functions/04-Exercise/task_01_smallest_of_three_numbers.py
+++ 04 Functions/04-Exercise/task_01_smallest_of_three_numbers.py
@@ -5,3 +5,9 @@ def get_smallest_number(number01, number02, number03):
 
 first_number, second_number, third_number = int(input()), int(input()), int(input())
 print(get_smallest_number(first_number, second_number, third_number))
+
+
+################################################   Task Description   ################################################
+# 1. Smallest of Three Numbers
+# Write a function that receives three integer numbers and returns the smallest. 
+# Print the result on the console. Use an appropriate name for the function.
