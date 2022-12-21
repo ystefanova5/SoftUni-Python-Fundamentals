f62929638cfb5a909ef5e36a6ec6bca0102383c3
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:10:36 +0200

    Update task_09_password_validator.py

--- 04 Functions/04-Exercise/task_09_password_validator.py
+++ 04 Functions/04-Exercise/task_09_password_validator.py
@@ -24,3 +24,16 @@ def password_validator(password):
 
 user_password = input()
 password_validator(user_password)
+
+
+################################################   Task Description   ################################################
+# 9. Password Validator
+# Write a function that checks if a given password is valid. Password validations are:
+#     • It should be 6 - 10 (inclusive) characters long
+#     • It should consist only of letters and digits
+#     • It should have at least 2 digits 
+# If a password is valid, print "Password is valid".
+# Otherwise, for every unfulfilled rule, print a message:
+#     • "Password must be between 6 and 10 characters"
+#     • "Password must consist only of letters and digits"
+#     • "Password must have at least 2 digits"
