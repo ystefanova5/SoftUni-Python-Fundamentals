2ac4ae3fa9d210bbad10d01b2c16a96048d49398
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:07:38 +0200

    Update task_05_even_numbers.py

--- 04 Functions/04-Exercise/task_05_even_numbers.py
+++ 04 Functions/04-Exercise/task_05_even_numbers.py
@@ -9,3 +9,9 @@ numbers_list = list(map(int, input().split(" ")))
 even_nums_filtering = filter(check_even_numbers, numbers_list)
 even_nums = list(even_nums_filtering)
 print(even_nums)
+
+
+################################################   Task Description   ################################################
+# 5. Even Numbers
+# Write a program that receives a sequence of numbers (integers) separated by a single space. 
+# It should print a list of only the even numbers. Use filter().
