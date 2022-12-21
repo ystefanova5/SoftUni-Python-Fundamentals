e1e50eb5af11790e82add6c9a15fed8a4b198cef
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-21 17:12:01 +0200

    Update task_11_loading_bar.py

--- 04 Functions/04-Exercise/task_11_loading_bar.py
+++ 04 Functions/04-Exercise/task_11_loading_bar.py
@@ -12,3 +12,11 @@ def get_loading_status(number):
 
 input_number = int(input())
 get_loading_status(input_number)
+
+
+################################################   Task Description   ################################################
+# 11. * Loading Bar
+# You will receive a single integer number between 0 and 100 (inclusive) 
+# divisible by 10 without remainder (0, 10, 20, 30...). 
+# Your task is to create a function that returns a loading bar depending on the number you have received in the input. 
+# Print the result on the console. For more clarification, see the examples below.
