7b55c8b98588c55e52218c7ace9b4bd29bb60c61
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-23 09:12:40 +0200

    Update task_07_rounding.py

--- 04 Functions/04-Labs/task_07_rounding.py
+++ 04 Functions/04-Labs/task_07_rounding.py
@@ -1,3 +1,9 @@
 numbers = input().split()
 output_list = list(map(lambda x: round(float(x)), numbers))
 print(output_list)
+
+
+################################################   Task Description   ################################################
+# 7. Rounding
+# Write a program that rounds all the given numbers, separated by a single space, and prints the result as a list. 
+# Use round().
