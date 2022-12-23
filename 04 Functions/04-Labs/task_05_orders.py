35f5f761f75b67fb9b354a0f4a6f798b079c5292
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-23 09:10:47 +0200

    Update task_05_orders.py

--- 04 Functions/04-Labs/task_05_orders.py
+++ 04 Functions/04-Labs/task_05_orders.py
@@ -14,3 +14,16 @@ def calculate_price(product, quantity):
 input_product = input()
 input_quantity = int(input())
 print(calculate_price(input_product, input_quantity))
+
+
+################################################   Task Description   ################################################
+# 5. Orders
+# Write a function that calculates the total price of an order and returns it. 
+# The function should receive one of the following products: 
+# "coffee", "coke", "water", or "snacks", and a quantity of the product. 
+# The prices for a single piece of each product are: 
+#     • coffee - 1.50
+#     • water - 1.00
+#     • coke - 1.40
+#     • snacks - 2.00
+# Print the result formatted to the second decimal place.
