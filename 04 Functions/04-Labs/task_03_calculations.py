88112a66d0cb64fe27a8df9da787fb674ffd4c07
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-23 09:08:30 +0200

    Update task_03_calculations.py

--- 04 Functions/04-Labs/task_03_calculations.py
+++ 04 Functions/04-Labs/task_03_calculations.py
@@ -1,8 +1,3 @@
-operation = input()
-parameter_a = int(input())
-parameter_b = int(input())
-
-
 def calculation(operator, a, b):
     result = None
     if operator == "multiply":
@@ -16,4 +11,15 @@ def calculation(operator, a, b):
     return result
 
 
+operation = input()
+parameter_a = int(input())
+parameter_b = int(input())
 print(calculation(operation, parameter_a, parameter_b))
+
+
+################################################   Task Description   ################################################
+# 3. Calculations
+# Create a function that receives three parameters, calculates a result depending on the given operator, and returns it. 
+# Print the result of the function.
+# The input comes as three parameters – an operator as a string and two integer numbers. 
+# The operator can be one of the following:  "multiply", "divide", "add", "subtract". 
