c397dd9b1332a875e9930c9bbdcf390ba558987f
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-23 09:09:48 +0200

    Update task_04_repeat_string.py

--- 04 Functions/04-Labs/task_04_repeat_string.py
+++ 04 Functions/04-Labs/task_04_repeat_string.py
@@ -2,3 +2,10 @@ input_string = input()
 num_repetitions = int(input())
 result = lambda string, repetitions: string * repetitions
 print(result(input_string, num_repetitions))
+
+
+################################################   Task Description   ################################################
+# 4. Repeat String
+# Write a function that receives a string and a counter n. 
+# The function should return a new string – the result of repeating the old string n times. 
+# Print the result of the function. Try using lambda.
