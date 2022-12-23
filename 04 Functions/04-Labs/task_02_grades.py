5f9f5a8cdc590ed217cf916a68040aa6a3849ee5
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-23 09:07:00 +0200

    Update task_02_grades.py

--- 04 Functions/04-Labs/task_02_grades.py
+++ 04 Functions/04-Labs/task_02_grades.py
@@ -16,3 +16,14 @@ def grade_as_text(grade):
 
 
 print(grade_as_text(grade_number))
+
+
+
+################################################   Task Description   ################################################
+# 2. Grades
+# Write a function that receives a grade between 2.00 and 6.00 and prints the corresponding grade in words.
+#     • 2.00 – 2.99 - "Fail"
+#     • 3.00 – 3.49 - "Poor"
+#     • 3.50 – 4.49 - "Good"
+#     • 4.50 – 5.49 - "Very Good"
+#     • 5.50 – 6.00 - "Excellent"
