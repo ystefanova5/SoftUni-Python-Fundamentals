a4797ae649dd43979c69c30cd5079041680e33aa
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-23 09:06:17 +0200

    Update task_01_absolute_values.py

--- 04 Functions/04-Labs/task_01_absolute_values.py
+++ 04 Functions/04-Labs/task_01_absolute_values.py
@@ -1,3 +1,9 @@
 numbers = list(map(float, input().split(" ")))
 abs_numbers = [abs(x) for x in numbers]
 print(abs_numbers)
+
+
+################################################   Task Description   ################################################
+1. Absolute Values
+Write a program that receives a sequence of numbers, separated by a single space, 
+and prints their absolute value as a list. Use abs().
