bd6ca050e91a579ab301b837aa510237fb7748fa
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-23 09:11:17 +0200

    Update task_06_calculate_rectangle_area.py

--- 04 Functions/04-Labs/task_06_calculate_rectangle_area.py
+++ 04 Functions/04-Labs/task_06_calculate_rectangle_area.py
@@ -5,3 +5,9 @@ def rectangle_area(width, height):
 input_width = int(input())
 input_height = int(input())
 print(rectangle_area(input_width, input_height))
+
+
+################################################   Task Description   ################################################
+# 6. Calculate Rectangle Area
+# Create a function that calculates and returns the area of a rectangle by given width and height. 
+# Print the result on the console.
