9529cb34fe868724fb5bcb9220e4f8f2079a8734
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-31 10:50:58 +0200

    Update task_02_center_point.py

--- 04 Functions/04-More-Exercise/task_02_center_point.py
+++ 04 Functions/04-More-Exercise/task_02_center_point.py
@@ -15,3 +15,13 @@ input_y1 = float(input())
 input_x2 = float(input())
 input_y2 = float(input())
 print(closest_point(input_x1, input_y1, input_x2, input_y2))
+
+
+################################################   Task Description   ################################################
+# 2. Center Point
+# You will be given the coordinates of two points 
+# on a Cartesian coordinate system - X1, Y1, X2, and Y2 on separate lines. 
+# Write a function that prints the point which is closest to the center 
+# of the coordinate system (0, 0) in the format: "({X}, {Y})"
+# If the points are at the same distance from the center, print only the first one. 
+# The resulting coordinates must be formatted to the lower integer.
