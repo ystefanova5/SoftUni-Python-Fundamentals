8d56f8f9fc4d5c6a9f50323b7437e86557bb9d01
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-31 10:52:01 +0200

    Update task_03_longer_line.py

--- 04 Functions/04-More-Exercise/task_03_longer_line.py
+++ 04 Functions/04-More-Exercise/task_03_longer_line.py
@@ -33,3 +33,13 @@ input_y3 = float(input())
 input_x4 = float(input())
 input_y4 = float(input())
 print(closest_point(longer_line(input_x1, input_y1, input_x2, input_y2, input_x3, input_y3, input_x4, input_y4)))
+
+
+################################################   Task Description   ################################################
+# 3. Longer Line
+# You will be given the coordinates of four points. The first and the second pair of points form two different lines. 
+# Create a function that prints the longer line in the format "({X1}, {Y1})({X2}, {Y2})" 
+# starting from the point which is closer to the center of the coordinate system (0, 0). 
+# You can reuse the method that you wrote for the previous problem. 
+# If the lines are of equal length, print only the first one. 
+# The resulting coordinates must be formatted to the lower integer.
