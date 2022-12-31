6726230845d4029dd777257d24ce3f954b978a21
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-31 10:52:42 +0200

    Update task_04_tribonacci_sequence.py

--- 04 Functions/04-More-Exercise/task_04_tribonacci_sequence.py
+++ 04 Functions/04-More-Exercise/task_04_tribonacci_sequence.py
@@ -19,3 +19,11 @@ def get_tribonacci_numbers(number):
 
 user_number = int(input())
 print(*get_tribonacci_numbers(user_number))
+
+
+################################################   Task Description   ################################################
+# 4. Tribonacci Sequence
+# In the Tribonacci sequence, every number is formed by the sum of the previous 3.
+# Write a function that prints numbers from the Tribonacci sequence on one line 
+# separated by a single space, starting from 1. 
+# You will receive a positive integer number as input.
