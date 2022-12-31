df18fe2ab6765edbbd27ebbe460baf716f6e070e
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2022-12-31 10:49:51 +0200

    Update task_01_data_types.py

--- 04 Functions/04-More-Exercise/task_01_data_types.py
+++ 04 Functions/04-More-Exercise/task_01_data_types.py
@@ -10,3 +10,13 @@ def data_types_operations(data_type, user_input):
 current_data_type = input()
 current_user_input = input()
 print(data_types_operations(current_data_type, current_user_input))
+
+
+################################################   Task Description   ################################################
+# 1. Data Types
+# Write a function that, depending on the first line of the input, 
+# reads one of the following strings: "int", "real", or "string".
+#     • If the data type is an int, multiply the number by 2.
+#     • If the data type is real, multiply the number by 1.5 and format the result to the second decimal point.
+#     • If the data type is a string, surround the input with "$".
+# Print the result on the console.
